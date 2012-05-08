2b53f492ee4b6d181133f37af0c1cea17c368c8b
admin <admin@10.6.0.141>
2012-05-08 19:33:51 -0300

    aaa

--- EDA-TPE/src/backEnd/Dictionary.java
+++ EDA-TPE/src/backEnd/Dictionary.java
@@ -1,12 +1,15 @@
 package backEnd;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class Dictionary {
 
 	Node[] trees = new Node[26];
+	Map<Character, List<Node>> charAppearences = new HashMap<Character, List<Node>> ();
 	
 	public void printAll(){
 		for(Node n : trees){
@@ -15,9 +18,14 @@ public class Dictionary {
 		}
 	}
 	
+	public void printAppearences() {
+		System.out.println(charAppearences);
+	}
+	
 	public void addWord(List<String> wordsList) {
 		for (String word : wordsList)
 			addWord(word);
+		updateAppearences();
 	}
 	
 	private void addWord(String word) {
@@ -33,6 +41,20 @@ public class Dictionary {
 		if (trees[offSet] == null)
 			trees[offSet] = new Node(firstChar);
 		trees[offSet].addWord(it);
+	}
+
+	private void updateAppearences(){
+		for (char i = 'A'; i <= 'Z'; i++){
+			charAppearences.put(i, new ArrayList<Node> ());
+		}
+		
+		for (Character c : charAppearences.keySet()){
+			for (Node n: trees) {
+				if (n != null)
+					n.getAppearencesOf(c, charAppearences.get(c));
+			}
+		}
+			
 	}
 	
 	private class Node {
@@ -86,6 +108,24 @@ public class Dictionary {
 				if (n != null)
 					n.printWords(strNew);
 		}
+		
+		public void getAppearencesOf(Character value, List<Node> list) {
+			
+			if (this.value.equals(value)) {
+				list.add(this);
+				return;
+			}
+			for (Node n: sons) {
+				if (n != null)
+					n.getAppearencesOf(value, list);
+			}
+			
+		}
+		
+		public String toString(){
+			return "Nodo con: " + value + "//";
+		}
+	
 	}
 	
 }
