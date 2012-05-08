508e12393a34f3eb9bda9eafdfffdf73544923e1
admin <admin@10.6.0.141>
2012-05-08 18:42:45 -0300

    jj

--- EDA-TPE/src/helpers/CharValues.java
+++ EDA-TPE/src/helpers/CharValues.java
@@ -1,47 +1,13 @@
 package helpers;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class CharValues {
 
-	Map<Character, Integer> valuesMap = new HashMap<Character, Integer> ();
+	int[] scores = {1,3,3,2,1,4,2,4,1,8,
+			5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
 	
-	public CharValues(String source) {
-		initializeValues(source);
-	}
-	
-	private void initializeValues(String source) {
-		File inputFile = new File(source);
-		BufferedReader inputWords;
-		
-		try {
-			inputWords = new BufferedReader(new FileReader(inputFile));
-		} catch (FileNotFoundException e) {
-			System.out.println("Archivo de puntajes no encontrado");
-			return;
-		}
-		
-		String line;
-		
-		try {
-			while ((line = inputWords.readLine()) != null){
-				Integer value = Integer.valueOf(line.charAt(1)) - '0';
-				valuesMap.put(line.charAt(0), value==0?10:value);
-			}
-		} catch (IOException e) {
-			System.out.println("Archivo invalido");
-		}
-		
-	}
-	
-	public Map<Character, Integer> getValues(){
-		return valuesMap;
+	public int[] getScores() {
+		return scores;
 	}
 	
 }
