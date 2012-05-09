a0d069892b93d9ec940346e9ce7f30fceb2cf3b1
Lucas Moscovicz <lukk.m@hotmail.com>
2012-05-09 17:51:33 -0300

    Cambiado Board x e y

--- EDA-TPE/src/backEnd/Board.java
+++ EDA-TPE/src/backEnd/Board.java
@@ -4,24 +4,24 @@ public class Board {
 
 	private char[][] board = new char[15][15];
 	
-	public boolean setWord(int startRow, int startCol, String word, Rotation rot){
-		if (startRow < 0 || startRow > board.length - 1 || startCol < 0 || startCol > board[0].length - 1)
+	public boolean setWord(int startX, int startY, String word, Rotation rot){
+		if (startX < 0 || startX > board.length - 1 || startY < 0 || startY > board[0].length - 1)
 			return false;
 		int length = word.length();
-		if (startRow + length * rot.getTop() > board.length - 1 || startCol + length * rot.getLeft() > board.length - 1)
+		if (startX + length * rot.getLeft() > board.length - 1 || startY + length * rot.getTop() > board.length - 1)
 			return false;
 		for (int i = 0; i < length; i++){
-			board[startRow + i * rot.getTop()][startCol + i * rot.getLeft()] = word.charAt(i); 
+			board[startX + i * rot.getLeft()][startY + i * rot.getTop()] = word.charAt(i); 
 		}
 		return true;
 	}
 	
-	public void setPosition(int row, int col, char c){
-		board[row][col] = c;
+	public void setPosition(int x, int y, char c){
+		board[x][y] = c;
 	}
 	
-	public char getPosition(int row, int col){
-		return board[row][col];
+	public char getPosition(int x, int y){
+		return board[x][y];
 	}
 	
 	
