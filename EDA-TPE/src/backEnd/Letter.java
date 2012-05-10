1cab672250e423b821c490b77e3ae99594bd8e59
admin <admin@admin-34ea83e9f>
2012-05-10 13:05:27 -0300

    Agregado hashCode e equals en Letter

--- EDA-TPE/src/backEnd/Letter.java
+++ EDA-TPE/src/backEnd/Letter.java
@@ -29,5 +29,37 @@ public class Letter {
 	public char getValue(){
 		return value;
 	}
+	
+	public int hashCode() {
+		final int prime = 277;
+		int result = 1;
+		result = prime * result + ((rot == null) ? 0 : rot.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Letter other = (Letter) obj;
+		if (rot != other.rot)
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
 
 }
